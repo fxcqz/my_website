5b7f18718e52c1e35f46adac23e6b97513a6ce86
matt <mhrawcliffe@gmail.com>
2019-06-04 20:40:52 +0100

    Make pages registerable and show posts on index page

--- source/app.d
+++ source/app.d
@@ -2,32 +2,59 @@ import std.stdio;
 
 import diet.html;
 
-static immutable string SOURCEPATH = "content/";
+// TODO load globals from file
+// TODO add styling
 
-struct GlobalsImpl {
+static immutable string SOURCEPATH = "content/";
+static immutable string TARGETPATH = "build/";
+
+struct GlobalsImpl
+{
   string WebsiteName = "Barely Laughing";
 }
 
 immutable GlobalsImpl GLOBALS = GlobalsImpl();
 
-struct Index {
-  string heading;
+struct Page(T) {
+  T obj;
+  string url;
+}
 
-  static Index parse(string path) {
-    return Index("b5.re");
+mixin template Register(T)
+{
+  static Page!(T)[] objs;
+  static bool parsed = false;
+}
+
+struct Index
+{
+  string heading;
+  Page!(Post)[] posts;
+
+  static Index parse(string path)
+  in (Post.parsed)
+  {
+    return Index("b5.re", Post.objs);
   }
 }
 
-struct Post {
+struct Post
+{
   string title;
   string[] content;
 
+  mixin Register!Post;
+
   static Post parse(string path)
+  in (!Post.parsed)
+  out (; Post.parsed)
   {
     import std.file : readText;
     import std.string : splitLines;
 
     string[] parts = readText(path).splitLines;
+    Post.parsed = true;
+
     return Post(parts[0], parts[1 .. $]);
   }
 }
@@ -48,23 +75,28 @@ string make_basepath(string suffix)
   return buildPath(SOURCEPATH, suffix);
 }
 
-void write_file(T, string TemplateFile)(string src_path, string basepath)
+void write_file(T, string TemplateFile)(string src_path)
 {
   import std.array : replace;
   import std.file : exists, mkdirRecurse, readText;
   import std.path : buildPath, dirName, setExtension;
 
-  string path = buildPath(basepath, src_path).replace(SOURCEPATH, "");
+  string path = buildPath(TARGETPATH, src_path).replace(SOURCEPATH, "").setExtension("html");
   string dirs = path.dirName;
 
   if (!dirs.exists) {
     mkdirRecurse(dirs);
   }
 
-  auto file = File(path.setExtension("html"), "wt");
+  auto file = File(path, "wt");
   auto dest = file.lockingTextWriter;
   T ctx = T.parse(src_path);
   dest.compileHTMLDietFile!(TemplateFile, ctx, GLOBALS);
+
+  static if (__traits(hasMember, T, "objs")) {
+    // register this page instance so it can be referenced elsewhere
+    T.objs ~= Page!T(ctx, path.replace(TARGETPATH, ""));
+  }
 }
 
 void main(string[] args)
@@ -75,17 +107,15 @@ void main(string[] args)
   // Output from build goes in build/
   // views/ is for diet templates
 
-  string target = "build/";
-
-  if (!target.exists) {
-    target.mkdir;
+  if (!TARGETPATH.exists) {
+    TARGETPATH.mkdir;
   }
 
   string[] posts = get_posts(make_basepath("posts/"));
 
   foreach (post; posts) {
-    write_file!(Post, "post.dt")(post, target);
+    write_file!(Post, "post.dt")(post);
   }
 
-  write_file!(Index, "index.dt")(make_basepath("index.txt"), target);
+  write_file!(Index, "index.dt")(make_basepath("index.txt"));
 }
